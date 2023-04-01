1366a15882eb701dab1fb9a53159aa83395779c3
Bharath <bharathlenin@gmail.com>
2023-04-01 16:47:08 +0530

    React remote

---
+++ react-remote-app4/src/components/DonutChart/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export interface Props {
+    border: string;
+}
+
+const DonutChart: React.FC<Props> = (props:Props) => {
+
+    return (
+        <div>test</div>
+    )
+}
+export {DonutChart};
